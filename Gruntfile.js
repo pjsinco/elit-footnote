e632e29bc1f64d6111550ae1c0fe476c789cc599
pjsinco <psinco@gmail.com>
2017-03-31 15:13:37 -0500

    First commit

---
+++ Gruntfile.js
@@ -0,0 +1,106 @@
+var files = {
+
+  vendor: {
+    scripts: [
+      //'vendor/scripts/imagesloaded.pkgd.min.js',
+      //'vendor/scripts/google-image-layout-modified.js',
+      'vendor/scripts/photoswipe-ui-default.min.js',
+      'vendor/scripts/photoswipe.js',
+    ],
+  },
+};
+
+module.exports = function(grunt) {
+
+
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+  
+    sass: {
+      options: {
+        sourceMap: true,
+      },
+      dev: {
+        files: {
+          'public/styles/<%= pkg.name %>.css': 'sass/style.scss',
+        },
+      },
+    },
+
+    concat: {
+      options: {
+          //stripBanners: true,
+      },
+      all: {
+        src: files.vendor.scripts,
+        dest: './public/scripts/<%= pkg.name %>.js',
+      },
+    },
+  
+    uglify: {
+      options: {
+        banner: '/* <%= pkg.name %> - v<%= pkg.version %> */'
+      },
+      dist: {
+        files: {
+          './public/scripts/<%= pkg.name %>.min.js': ['<%= concat.all.dest %>']
+        }
+      }
+    },
+
+    notify: {
+      sass: {
+        options: {
+          title: 'Sass',
+          message: 'Sassed!',
+        },
+      },
+      scripts: {
+        options: {
+          title: 'Scripts',
+          message: 'Processed!',
+        }
+      }
+    },
+
+    autoprefixer: {
+      css: {
+        src: 'public/styles/<%= pkg.name %>.css',
+        options: {
+          browsers: [
+            '> 1%',
+            'last 2 versions',
+            'Firefox ESR',
+            'iOS >= 7',
+            'ie >= 10'
+          ],
+        },
+      },
+    },
+
+    watch: {
+      scripts: {
+        files: ['vendor/scripts/**/*.js'],
+        tasks: ['concat', 'notify:scripts'],
+      },
+      sass: {
+        files: ['sass/**/*.scss'],
+        tasks: ['sass:dev', 'notify:sass', 'autoprefixer:css' ],
+      },
+    },
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-notify');
+  grunt.loadNpmTasks("grunt-autoprefixer");
+
+  grunt.registerTask('scripts-dev', ['concat']);
+  grunt.registerTask('scripts-dist', ['concat', 'uglify']);
+  grunt.registerTask('compile-sass', ['sass:dev', 'notify:sass']);
+  grunt.registerTask('default', ['watch']);
+};
---
+++ elit-footnote.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Plugin Name: Elit Footnote
+ * Plugin URI: https://github.com/pjsinco/elit-footnote
+ * Description: Display footnotes
+ * Version: 0.1.0
+ * Author: Patrick Sinco
+ * License: GPL2
+ */
+
+if ( !defined( 'WPINC' ) ) {
+  die;
+}
